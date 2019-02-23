344bc45f5ea7c222e544e646fb463fa3d1640d53
Mussie Tecle <filago14@gmail.com>
2019-02-23 14:46:40 +0000

    I.write-test

--- I.write-tests/06-largest-number/largest-number.test.js
+++ I.write-tests/06-largest-number/largest-number.test.js
@@ -1,3 +1,19 @@
+var getLargestNumbers = require('./Largest-number');
+
+test('Get largest-number', function () {
+
+    // Arrange
+    const input = [3, 21, 88, 4, 36];
+    const expection = 88;
+
+    // Act
+    const result = getLargestNumbers(input);
+
+    // Assert
+    expect(result).toEqual(expection);
+
+});
+
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
--- I.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ I.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,20 @@
+var getEvenNumbers = require('./get-even-numbers');
+
+test('Get getEven', function () {
+
+    // Arrange
+    const input = [22, 13, 73, 82, 4];
+    const expection = [22, 82, 4];
+
+    // Act
+    const result = getEvenNumbers(input);
+
+    // Assert
+    expect(result).toEqual(expection);
+
+});
+
+
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
